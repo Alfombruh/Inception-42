f9c123fb2b5ab4c1bf64acc86dd957a2b6f18c29
jofernan <jofernan@jofernan.jofernan.42.fr>
2023-03-30 17:48:45 +0100

    done

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -16,6 +16,8 @@ wp --path=/var/www/html config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQ
 
 wp core install --path=/var/www/html  --url=${DOMAIN_NAME} --title="Sabes que en LLeida dicen la A con fonetica de E" --admin_name=${WP_ADMIN} --admin_user=${WP_ADMIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="jofernan@student.42urduliz.com" --skip-email --allow-root
 
+wp user create --allow-root --path=/var/www/html ${WP_USER} juanito@hotmail.com --user_pass=${WP_PASSWORD} --role="author"
+
 wp --path=/var/www/html theme install https://downloads.wordpress.org/theme/twentytwentythree.1.0.zip --allow-root
 
 wp --path=/var/www/html theme activate twentytwentythree --allow-root
